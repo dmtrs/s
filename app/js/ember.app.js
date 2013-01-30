b89db81ae13858da824ff03115a1ddc8469efe88
dmtrs <tydeas.dr@gmail.com>
2013-01-31 01:44:20 +0200

    Add ember.app.js to the application

---
+++ app/js/ember.app.js
@@ -0,0 +1,12 @@
+window.App = Ember.Application.create();
+window.App.Router.map(function() {
+    this.route("about");
+});
+window.App.AboutController = Ember.Controller.extend({
+    fname: 'About!!'
+});
+window.App.AboutRoute = Ember.Route.extend({
+    renderTemplate: function() {
+        this.render('about-template');
+    }
+});

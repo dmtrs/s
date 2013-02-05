03ba1faf91497b5e0c39c7bfe01673a85ea8bb1b
dmtrs <tydeas.dr@gmail.com>
2013-02-06 00:59:18 +0200

    Digged the surface

--- app/index.php
+++ app/index.php
@@ -1,24 +1,12 @@
 <?php
 $config = require_once __DIR__.'/../config/main.php';
-//functions
-function get($url, $params) {
-    $ch = curl_init();
-    $url = $url.'?'.http_build_query($params);
-    error_log($url);
-
-    curl_setopt($ch, CURLOPT_URL, $url );
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, '3');
-    $content = curl_exec($ch);
-    curl_close($ch);
-    return $content;
-}
 //applcation
 require __DIR__.'/../lib/slim/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
+$app->config($config);
+
 $app->setName('Web application');
 
 $app->get('/', function () use ($app) {
@@ -27,19 +15,45 @@ $app->get('/', function () use ($app) {
     ));
 });
 
-$app->get('/:blog', function($blog) use ($app) {
+$app->get('/:blog(/:post)', function($blog, $post=null) use ($app) {
     $r = $app->response();
     $r['Content-Type'] = 'application/json';
 
-    $source = __DIR__.'/../data/'.$blog.'.json';
+    echo json_encode(retrieve($blog, $post, $app->config('components.tumblr.api_key')));
+})->conditions(array('blog' => '\w+\.tumblr\.com'));
+
+$app->run();
+//functions
+function get($url, $params=array(), $api_key=null) {
+    
+    $ch = curl_init();
+    $url = $url.'?'.http_build_query($params);
+
+    curl_setopt($ch, CURLOPT_URL, $url );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, '3');
+
+    $content = curl_exec($ch);
+    curl_close($ch);
+    return $content;
+}
+
+function retrieve($blog, $post=null, $api_key=null) {
+    $source = __DIR__.'/../data/'.$blog.'.'.(($post!==null)?$post:'').'.json';
     if(!isset($_GET['debug']) && file_exists($source)) {
         $content = file_get_contents($source);
     } else {
-        $content = get('http://api.tumblr.com/v2/blog/'.$blog.'/posts', array(
-            'api_key'     => 'fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4',
+        $params = array(
             'notes_info'  => 'true',
             'reblog_info' => 'true',
-        ));
+        );
+        if($api_key!==null)
+            $params['api_key'] = $api_key;
+
+        if($post!==null)
+            $params['id']=$post;
+        
+        $content = get('http://api.tumblr.com/v2/blog/'.$blog.'/posts', $params);
         file_put_contents($source, $content);
     }
     $provider = (object)json_decode($content);
@@ -48,12 +62,13 @@ $app->get('/:blog', function($blog) use ($app) {
 
     $posts = $provider->posts;
 
-    echo json_encode(array_map(function($p) {
+    return array_map(function($p) {
         $post = (object)array(
             'id'          => $p->id,
             'blog_name'   => $p->blog_name,
             'notes_count' => $p->note_count,
             'timestamp'   => $p->timestamp,
+            'url'         => implode('/',array('http://staging.stamblr.com', $p->blog_name.'.tumblr.com', $p->id)),
         );
         
         if(isset($p->notes)) {
@@ -62,7 +77,7 @@ $app->get('/:blog', function($blog) use ($app) {
                     'id' => $reblog->post_id,
                     'blog_name' => $reblog->blog_name,
                     'timestamp' => $reblog->timestamp,
-                    'url'       => implode('/',array('http://staging.stamblr.com', $post->blog_name, $reblog->post_id)),
+                    'url'       => implode('/',array('http://staging.stamblr.com', $reblog->blog_name.'.tumblr.com', $reblog->post_id)),
                 );
                 return $r;
             }, array_filter($p->notes,function($note) {
@@ -70,9 +85,5 @@ $app->get('/:blog', function($blog) use ($app) {
             }));
         }
         return $post;
-    }, $provider->posts));
-})->conditions(array(
-    'blog' => '\w+\.tumblr\.com'
-));
-
-$app->run();
+    }, $provider->posts);
+}
--- config/main-example.php
+++ config/main-example.php
@@ -1,3 +1,4 @@
 <?php
 return array(
+    'components.tumblr.api_key' => 'tumblr api key'
 );

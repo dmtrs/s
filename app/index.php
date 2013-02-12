003203869204c6cd0edd6d33eca65abbfd7b94a0
dmtrs <tydeas.dr@gmail.com>
2013-02-12 21:41:07 +0200

    Removed mustache-php engine

--- app/index.php
+++ app/index.php
@@ -3,12 +3,8 @@ $config = require_once __DIR__.'/../config/main.php';
 require __DIR__.'/../lib/slim/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
-require __DIR__.'/../lib/slim-extras/Views/Mustache.php';
-\Slim\Extras\Views\Mustache::$mustacheDirectory = __DIR__.'/../lib/mustache/src/Mustache';
+$app = new \Slim\Slim(array());
 
-$app = new \Slim\Slim(array(
-    'view' => new \Slim\Extras\Views\Mustache()
-));
 $app->setName('Web application');
 
 $app->get('/', function () use ($app) {
